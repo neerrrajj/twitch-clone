d954ed08a882e1d38ed934b01a71a413bc2584d1
neerrrajj <neerrrajj@outlook.com>
2024-08-16 14:02:57 +0530

    auth setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <main className="text-rose-500 font-bold">helo</main>;
+  return (
+    <div className="text-rose-500 font-bold">
+      <p>helo</p>
+      <UserButton />
+    </div>
+  );
 }
